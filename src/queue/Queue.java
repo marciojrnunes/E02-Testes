457c0cb9ae8b2c7531ccdfd852ddf11276f9c77f
KarenGiullia <138712215+KarenGiullia@users.noreply.github.com>
2024-05-17 15:25:18 -0300

    Entrga das atividades

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,6 +1,26 @@
 package queue;
 
 public class Queue<Trem> {
+    private LinkedList<Trem> fila;
 
+    public Queue() {
+        this.fila = new LinkedList<>();
+    }
+
+    public boolean estaVazia() {
+        return this.fila.isEmpty();
+    }
+    public void enfileirar(Trem item) {
+        this.fila.add(item);
+    }
+    public int tamanho() {
+        return this.fila.size();
+    }
+    public Trem desenfileirar() {
+        if (this.estaVazia()) {
+            return null;
+        }
+        return this.fila.remove();
+    }
 
 }
--- src/test/StackTest.java
+++ src/test/StackTest.java
@@ -6,5 +6,45 @@ import stack.Stack;
 import static org.junit.Assert.*;
 
 public class StackTest {
+    private Stack<Integer> stack;
 
+    @org.junit.Before
+    public void setUp() {
+        stack = new Stack<>();
+    }
+
+    @Test
+    public void testPilhaEstaVazia() {
+        assertTrue("A pilha deve estar vazia inicialmente.",stack.estaVazia());
+    }
+
+    @Test
+    public void testPilhaNaoEstaVazia() {
+        stack.empilhar(1);
+        assertFalse("A pilha não deve estar vazia após empilhar um elemento.",stack.estaVazia());
+    }
+
+    @Test
+    public void testEmpilharElementos() {
+        stack.empilhar(1);
+        stack.empilhar(2);
+        assertEquals(String.valueOf(2), stack.tamanho(), "A pilha deve conter 2 elementos.");
+    }
+
+    @Test
+    public void testDesempilharElementos() throws Exception {
+        stack.empilhar(1);
+        stack.empilhar(2);
+        int elemento = stack.desempilhar();
+        assertEquals(String.valueOf(2), elemento, "O elemento desempilhado deve ser 2.");
+        assertEquals(String.valueOf(1), stack.tamanho(), "A pilha deve conter 1 elemento após desempilhar.");
+    }
+
+    @Test
+    public void testDesempilharPilhaVazia() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            stack.desempilhar();
+        });
+        assertEquals("A pilha está vazia!", exception.getMessage(), "Deve retornar: 'A pilha está vazia!'");
+    }
 }
